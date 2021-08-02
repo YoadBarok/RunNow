958ff269c56a1b692056a6ac205d3de02320bf9c
YoadBarok <yoadbarok@hotmail.com>
2021-08-02 23:14:07 +0200

    update env vars

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///races-database.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-app.secret_key = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY", "Eden My Love")
 Bootstrap(app)
 
 
